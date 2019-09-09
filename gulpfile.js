5e32a2d7e5a8482be46c7e61b3312474f8a647a1
Elena Botvinnikova <botvinnikova90@mail.ru>
2019-09-10 05:34:54 +1000

    Исправление ошибок для защиты

--- gulpfile.js
+++ gulpfile.js
@@ -16,27 +16,6 @@ var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
 
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
-});
-
-gulp.task("clean", function () {
-  return del("build");
-});
-
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source/*.ico"
-    ], {
-      base: "source"
-    })
-    .pipe(gulp.dest("build"));
-});
-
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -85,6 +64,30 @@ gulp.task("images", function () {
   .pipe(gulp.dest("source/img"));
 });
 
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/css/**"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"));
+
 gulp.task("server", function () {
   server.init({
     server: "build/",
@@ -99,5 +102,9 @@ gulp.task("server", function () {
   gulp.watch("source/*.html"), gulp.series("html", "refresh");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+  });
+
 gulp.task("start", gulp.series("build", "server"));
